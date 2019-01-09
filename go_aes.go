a640b512596dc2d3320a67546d1ab8e4964a9fdd
Lucas Löffel <lucas@loeffel.io>
2019-01-09 13:38:30 +0100

    init

---
+++ go_aes.go
@@ -0,0 +1,58 @@
+package go_aes
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+)
+
+func getCipherBlock(key []byte) (cipher.Block, error) {
+	return aes.NewCipher(key)
+}
+
+func getGCM(block cipher.Block) (cipher.AEAD, error) {
+	return cipher.NewGCM(block)
+}
+
+func Encrypt(data []byte, key []byte) ([]byte, error) {
+	var (
+		block cipher.Block
+		gcm   cipher.AEAD
+		err   error
+	)
+
+	if block, err = getCipherBlock(key); err != nil {
+		return nil, err
+	}
+
+	if gcm, err = getGCM(block); err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	return gcm.Seal(nonce, nonce, data, nil), nil
+}
+
+func Decrypt(data []byte, key []byte) ([]byte, error) {
+	var (
+		block cipher.Block
+		gcm   cipher.AEAD
+		err   error
+	)
+
+	if block, err = getCipherBlock(key); err != nil {
+		return nil, err
+	}
+
+	if gcm, err = getGCM(block); err != nil {
+		return nil, err
+	}
+
+	return gcm.Open(nil, data[:gcm.NonceSize()], data[gcm.NonceSize():], nil)
+}
---
+++ go_aes_test.go
@@ -0,0 +1,51 @@
+package go_aes
+
+import (
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestEncryptAndDecrypt(t *testing.T) {
+	tests := []struct {
+		description          string
+		key                  []byte
+		data                 []byte
+		expectedData         []byte
+		expectedEncryptError error
+		expectedDecryptError error
+	}{
+		{
+			description:          "valid",
+			key:                  []byte("{HtJt8;L2e7KVv6f3#TUyPJQ2p/cjw.="),
+			data:                 []byte("my-password"),
+			expectedData:         []byte("my-password"),
+			expectedEncryptError: nil,
+			expectedDecryptError: nil,
+		},
+		{
+			description:          "invalid key size (48)",
+			key:                  []byte("PpNYoA8@imQWWMvM=Zf;;72^DRYij2nU]dcV2fAoCav2uaUw"),
+			data:                 []byte("my-password-2"),
+			expectedData:         nil,
+			expectedEncryptError: errors.New("crypto/aes: invalid key size 48"),
+			expectedDecryptError: errors.New("crypto/aes: invalid key size 48"),
+		},
+	}
+
+	for _, test := range tests {
+		encrypted, err := Encrypt(test.data, test.key)
+
+		if err != nil {
+			assert.EqualError(t, err, test.expectedEncryptError.Error(), "Encrypt error should be equal")
+		}
+
+		decrypted, err := Decrypt(encrypted, test.key)
+
+		if err != nil {
+			assert.EqualError(t, err, test.expectedDecryptError.Error(), "Decrypt error should be equal")
+		}
+
+		assert.Equal(t, test.expectedData, decrypted)
+	}
+}
